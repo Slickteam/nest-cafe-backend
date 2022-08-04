c2d46da4520cba054afaf7990e1125fa5d44ebde
Ronan MOREL <ronan.morel@slickteam.fr>
2022-08-04 17:24:08 +0200

    chore: add third parties service

---
+++ .docker/keycloak-init.sql
@@ -0,0 +1,2 @@
+CREATE USER keycloak WITH PASSWORD 'keycloak123';
+CREATE DATABASE keycloak OWNER keycloak;
---
+++ .docker/nestcafe-init.sql
@@ -0,0 +1,2 @@
+CREATE USER nestcafe WITH PASSWORD 'nestcafe123';
+CREATE DATABASE nestcafe OWNER nestcafe;
